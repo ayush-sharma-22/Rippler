c9e576f76f8b7d5df8f32d9b1d7a5d686245a5d1
ayush-sharma-22 <kennyack001@gmail.com>
2026-06-24 21:24:24 +0530

    fixed the origin patterns

--- backend/src/main/java/com/ayush/rippler/controller/AnalysisController.java
+++ backend/src/main/java/com/ayush/rippler/controller/AnalysisController.java
@@ -11,7 +11,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api/analyze")
-@CrossOrigin(origins = "*")
+@CrossOrigin(originPatterns = "*")
 @RequiredArgsConstructor
 public class AnalysisController {
 
